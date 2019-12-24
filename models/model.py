c1cd9595c993952f465f58c05869f3a4d0d80d77
xetxezarreta <xabi1500@gmail.com>
2019-12-24 12:35:06 +0100

    commit

--- models/model.py
+++ models/model.py
@@ -1,6 +1,72 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
+class ReconstructionErrorModel(object):
+    def __init__(self, train_data, model=PCA(n_components=0.95)):
+        """
+        Constructor
+
+        Parameters
+        ----------
+        train_data: Data used to train component analisys model.
+        model: Any sklearn component analisys algorithm.       
+        """
+        self.model = model.fit(train_data)
+
+    def predict(self, data):
+        """
+        It does the inverse transformation with the component analysis algorithm, 
+        returning the reconstruction error of each instance.
+
+        Parameters
+        ----------
+        data: Instances to make prediction.         
+
+        Returns
+        -------
+        Prediction of the instances.
+        """
+        rec_data = self.__reconstruct_data(data)
+        loss = self.__get_projection_loss(data, rec_data)
+        error_list = []
+        for row in loss:
+            error = row.mean()            
+            error_list.append(error)      
+        return np.asarray(error_list)
+    
+    def __get_projection_loss(self, in_data, out_data):   
+        """
+        Calculates the projection loss between input and output data.
+
+        Parameters
+        ----------
+        in_data: Input/Original data.       
+        out_data: Output/Reconstructed data.   
+
+        Returns
+        -------
+        Projection loss.
+        """        
+        return ((in_data - out_data) ** 2)
+
+    def __reconstruct_data(self, data):
+        """
+        Perform the transform and the inverse transform of the data, 
+        trying to reconstruct the original data.
+
+        Parameters
+        ----------
+        data: Input/Original data.       
+
+        Returns
+        -------
+        Output/Reconstructed data.  
+        """   
+        data_transformed = self.model.transform(data)  
+        data_reconstructed = self.model.inverse_transform(data_transformed)
+        return data_reconstructed
+
+'''
 class ca_xai_classification_model(object):
     def __init__(self, train_data, anomaly_threshold, model=PCA(n_components=0.95)):
         """
@@ -64,61 +130,4 @@ class ca_xai_classification_model(object):
         data_transformed = self.model.transform(data)  
         data_reconstructed = self.model.inverse_transform(data_transformed)
         return data_reconstructed
-
-class ca_xai_regression_model(object):
-    def __init__(self, train_data, model=PCA(n_components=0.95)):
-        """
-        Constructor
-
-        Parameters:
-        train_data: Data used to train component analisys model.
-        model: Any sklearn component analisys algorithm.       
-        """
-        self.model = model.fit(train_data)
-
-    def predict(self, data):
-        """
-        It does the inverse transformation with the component analysis algorithm, 
-        returning the reconstruction error of each instance.
-
-        Parameters:
-        data: Instances to make prediction.         
-
-        Returns:
-        Prediction of the instances.
-        """
-        rec_data = self.__reconstruct_data(data)
-        loss = self.get_projection_loss(data, rec_data)
-        error_list = []
-        for row in loss:
-            error = row.mean()            
-            error_list.append(error)      
-        return np.asarray(error_list)
-    
-    def get_projection_loss(self, in_data, out_data):   
-        """
-        Calculates the projection loss between input and output data.
-
-        Parameters:
-        in_data: Input/Original data.       
-        out_data: Output/Reconstructed data.   
-
-        Returns:
-        Projection loss.
-        """        
-        return ((in_data - out_data) ** 2)
-
-    def __reconstruct_data(self, data):
-        """
-        Perform the transform and the inverse transform of the data, 
-        trying to reconstruct the original data.
-
-        Parameters:
-        data: Input/Original data.       
-
-        Returns:
-        Output/Reconstructed data.  
-        """   
-        data_transformed = self.model.transform(data)  
-        data_reconstructed = self.model.inverse_transform(data_transformed)
-        return data_reconstructed 
+'''
