095fcf65a396f6909cfb6d54f55a398fffa3906b
xetxezarreta <xabi1500@gmail.com>
2020-03-12 20:34:50 +0100

    commit

--- models/hotelling/t2.py
+++ models/hotelling/t2.py
@@ -8,3 +8,29 @@ def hotelling_tsquared(components):
     w = np.linalg.solve(cov, x)
     t2 = (x * w).sum(axis=0)
     return t2
+
+def hotelling_tsquared_v2(X, num_components=10):
+    import pandas as pd
+    from scipy import linalg
+    from scipy.special import gammaln
+    from scipy.sparse import issparse
+    from scipy import linalg
+    from sklearn.utils.extmath import svd_flip
+    from sklearn.preprocessing import StandardScaler
+    from scipy.spatial.distance import mahalanobis
+    
+    [U,s,V] = linalg.svd(X);     #diagonalisation
+    P = V[:,0:num_components]
+    I = np.diag(s)
+    I = I[0:num_components,0:num_components]
+    hotelling_t2 = np.zeros(X.shape[0])
+
+    for i in range(X.shape[0]):
+        a = X[i,:].dot(P)
+        b = a.dot(np.linalg.inv(I))
+        c = b.dot(P.T)
+        d = c.dot(X[i,:].T)
+        hotelling_t2[i] = d  
+        
+    return hotelling_t2
+    
