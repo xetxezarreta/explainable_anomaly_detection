b230a55316534d59e4b6295389bb8b9af5d7f15b
xetxezarreta <xabi1500@gmail.com>
2020-03-13 09:32:38 +0100

    commit

--- models/hotelling/t2.py
+++ models/hotelling/t2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.stats import f as f_distrib
+from scipy import linalg
 
 def hotelling_tsquared(components):
     x = components.transpose()    
@@ -9,28 +9,30 @@ def hotelling_tsquared(components):
     t2 = (x * w).sum(axis=0)
     return t2
 
-def hotelling_tsquared_v2(X, num_components=10):
-    import pandas as pd
-    from scipy import linalg
-    from scipy.special import gammaln
-    from scipy.sparse import issparse
-    from scipy import linalg
-    from sklearn.utils.extmath import svd_flip
-    from sklearn.preprocessing import StandardScaler
-    from scipy.spatial.distance import mahalanobis
-    
-    [U,s,V] = linalg.svd(X);     #diagonalisation
+def hotelling_and_qres(X, num_components):
+    [U,s,V] = linalg.svd(X)
     P = V[:,0:num_components]
     I = np.diag(s)
     I = I[0:num_components,0:num_components]
-    hotelling_t2 = np.zeros(X.shape[0])
 
+    # hotelling t2
+    hotelling_t2 = np.zeros(X.shape[0])
     for i in range(X.shape[0]):
         a = X[i,:].dot(P)
         b = a.dot(np.linalg.inv(I))
         c = b.dot(P.T)
         d = c.dot(X[i,:].T)
         hotelling_t2[i] = d  
-        
-    return hotelling_t2
+    
+    # q residuals
+    qres = np.zeros(X.shape[0])
+    for i in range(X.shape[0]):
+        PPt = P.dot(P.T)
+        identity = np.identity(PPt.shape[0])
+        I_PPt = identity-PPt
+        a = X[i,:].dot(I_PPt)
+        b = a.dot(X[i,:].T)
+        qres[i] = b
+
+    return hotelling_t2, qres
     
